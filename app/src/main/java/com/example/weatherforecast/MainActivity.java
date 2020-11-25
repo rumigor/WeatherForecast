2e770a8d067cfc7bc171600d1774ae99bed6652a
rumigor <rumigor@gmail.com>
2020-11-25 17:51:33 +0300

    FireBase2

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.weatherforecast;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Build;
@@ -15,7 +21,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,10 +29,12 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.weatherforecast.model.OnDialogListener;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
@@ -43,13 +50,12 @@ public class MainActivity extends AppCompatActivity
     public static String cityName;
     private WirelessConnectionLost wirelessConnectionLost;
     private SharedPreferences sharedPref;
-    // Имя ACTION для Broadcast, по нему Receiver и будет определяться
+    private float lng;
+    private float lat;
     private static final String ACTION_SEND_MSG = "android.intent.action.BATTERY_LOW";
-    // Имя передаваемого параметра
     private static final String NAME_MSG = "MSG";
-    // Эта константа спрятана в Intent классе,
-    // но именно при помощи ее можно поднять приложение
     public static final int FLAG_RECEIVER_INCLUDE_BACKGROUND = 0x01000000;
+    private static final int PERMISSION_REQUEST_CODE = 10;
 
 
     CurrentWeatherFragment currentWeatherFragment;
@@ -67,6 +73,7 @@ public class MainActivity extends AppCompatActivity
         Metrics metrics = Metrics.getInstance();
         metrics.setFahrenheit(sharedPref.getBoolean("METRICS", false));
         setContentView(R.layout.activity_main);
+        requestPemissions();
         Toolbar toolbar = initToolbar();
         initDrawer(toolbar);
         cityName = sharedPref.getString(CITY_NAME, null);
@@ -144,21 +151,26 @@ public class MainActivity extends AppCompatActivity
         goToWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String url="";
-                if (city.equals(getString(R.string.spb))){
-                    url = "https://www.gismeteo.ru/weather-sankt-peterburg-4079/";
-                }
-                else if (city.equals(getString(R.string.vln))){
-                    url = "https://www.gismeteo.ru/weather-vilnius-4230/";
-                }
-                else if (city.equals(getString(R.string.bcn))){
-                    url = "https://www.gismeteo.ru/weather-barcelona-1948/";
-                }
-                else url = "https://www.gismeteo.ru/";
-                Uri uri = Uri.parse(url);
-                Intent browser = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(browser);
-
+//                String url="";
+//                if (city.equals(getString(R.string.spb))){
+//                    url = "https://www.gismeteo.ru/weather-sankt-peterburg-4079/";
+//                }
+//                else if (city.equals(getString(R.string.vln))){
+//                    url = "https://www.gismeteo.ru/weather-vilnius-4230/";
+//                }
+//                else if (city.equals(getString(R.string.bcn))){
+//                    url = "https://www.gismeteo.ru/weather-barcelona-1948/";
+//                }
+//                else url = "https://www.gismeteo.ru/";
+//                Uri uri = Uri.parse(url);
+//                Intent browser = new Intent(Intent.ACTION_VIEW, uri);
+//                startActivity(browser);
+                  MapsFragment mapsFragment = new MapsFragment();
+                  getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.mainFragment, mapsFragment)
+                        .addToBackStack("MAPS_FRAGMENT")
+                        .commit();
             }
         });
     }
@@ -320,6 +332,89 @@ public class MainActivity extends AppCompatActivity
                         Log.d("PUSH", token);
                     }
                 });
+    }
+    private void requestPemissions() {
+        // Проверим, есть ли Permission’ы, и если их нет, запрашиваем их у
+        // пользователя
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Запрашиваем координаты
+            requestLocation();
+        } else {
+            // Permission’ов нет, запрашиваем их у пользователя
+            requestLocationPermissions();
+        }
+    }
+    // Запрашиваем координаты
+    private void requestLocation() {
+        // Если Permission’а всё- таки нет, просто выходим: приложение не имеет
+        // смысла
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            return;
+        // Получаем менеджер геолокаций
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+        // Получаем наиболее подходящий провайдер геолокации по критериям.
+        // Но определить, какой провайдер использовать, можно и самостоятельно.
+        // В основном используются LocationManager.GPS_PROVIDER или
+        // LocationManager.NETWORK_PROVIDER, но можно использовать и
+        // LocationManager.PASSIVE_PROVIDER - для получения координат в
+        // пассивном режиме
+        String provider = locationManager.getBestProvider(criteria, true);
+        if (provider != null) {
+            // Будем получать геоположение через каждые 10 секунд или каждые
+            // 10 метров
+            locationManager.requestLocationUpdates(provider, 3600000, 10000, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    lat = (float)location.getLatitude(); // Широта
+                    lng = (float)location.getLongitude(); // Долгота
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                }
+            });
+        }
+    }
+
+    // Запрашиваем Permission’ы для геолокации
+    private void requestLocationPermissions() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
+            // Запрашиваем эти два Permission’а у пользователя
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+    // Результат запроса Permission’а у пользователя:
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {   // Запрошенный нами
+            // Permission
+            if (grantResults.length == 2 &&
+                    (grantResults[0] == PackageManager.PERMISSION_GRANTED || grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+                // Все препоны пройдены и пермиссия дана
+                // Запросим координаты
+                requestLocation();
+            }
+        }
     }
 
 }
